9b907810231c6505bd82ad85dcbac9dbd1ba26d0
judicialbaran <barankahvecidev@gmail.com>
2023-01-01 00:13:48 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors:{
+        'arkaplan':'#202035',
+        'mor':'#786CD5',
+        'kutular':'#1B1B30',
+        'yazi':'#AAAAC5',
+        'payment':'#251F35',
+      }
+    },
+  },
+  plugins: [],
+}
